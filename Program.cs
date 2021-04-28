6ee3da985910a7fd72aad9c02342babefaa8bb1e
benforcapita <benforcapita@gmail.com>
2021-04-28 18:01:00 +0300

    Second solution
    By using interface

--- Program.cs
+++ Program.cs
@@ -3,28 +3,57 @@
 namespace Circular_reference_dependency_solutions
 {
 
-    public class ClassA
+    // public class ClassA
+    // {
+    //     public void callFriends(object sender, EventArgs e)
+    //     {
+    //         Console.WriteLine("calling friends");
+    //     }
+    // }
+
+    // public class ClassB
+    // {
+    //     public ClassA a_Instance = new ClassA();
+    //     public event EventHandler callingFriends;
+
+    //     public ClassB()
+    //     {
+    //         callingFriends += a_Instance.callFriends;
+    //     }
+
+    //     public void maikingCoffee()
+    //     {
+    //         Console.WriteLine("makingCoffee");
+    //         callingFriends.Invoke(this, EventArgs.Empty);
+    //     }
+    // }
+
+    public interface callingFriends
     {
-        public void callFriends(object sender, EventArgs e)
+        void callFriends();
+
+    }
+    public interface makingCoffee
+    {
+        void makeCoffee();
+
+    }
+
+    public class ClassA : callingFriends
+    {
+        public void callFriends()
         {
             Console.WriteLine("calling friends");
         }
     }
 
-    public class ClassB
+    public class ClassB : makingCoffee
     {
-        public ClassA a_Instance = new ClassA();
-        public event EventHandler callingFriends;
-
-        public ClassB()
-        {
-            callingFriends += a_Instance.callFriends;
-        }
-
-        public void maikingCoffee()
+        public callingFriends i_instance = new ClassA();
+        public void makeCoffee()
         {
             Console.WriteLine("makingCoffee");
-            callingFriends.Invoke(this, EventArgs.Empty);
+            i_instance.callFriends();
         }
     }
     class Program
@@ -32,7 +61,7 @@ namespace Circular_reference_dependency_solutions
         static ClassB instance = new ClassB();
         static void Main(string[] args)
         {
-            instance.maikingCoffee();
+            instance.makeCoffee();
         }
     }
 }
