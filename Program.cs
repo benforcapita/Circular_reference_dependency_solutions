cb00cab0b1e16da68c8ee1185a6e1bd46e8029da
benforcapita <benforcapita@gmail.com>
2021-04-28 14:53:16 +0300

    First solution
    Use event handler in order to subscribe to the method in class A and invoke it

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Circular_reference_dependency_solutions
+{
+
+    public class ClassA
+    {
+        public void callFriends(object sender, EventArgs e)
+        {
+            Console.WriteLine("calling friends");
+        }
+    }
+
+    public class ClassB
+    {
+        public ClassA a_Instance = new ClassA();
+        public event EventHandler callingFriends;
+
+        public ClassB()
+        {
+            callingFriends += a_Instance.callFriends;
+        }
+
+        public void maikingCoffee()
+        {
+            Console.WriteLine("makingCoffee");
+            callingFriends.Invoke(this, EventArgs.Empty);
+        }
+    }
+    class Program
+    {
+        static ClassB instance = new ClassB();
+        static void Main(string[] args)
+        {
+            instance.maikingCoffee();
+        }
+    }
+}
